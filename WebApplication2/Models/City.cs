8f53131a773626332a2156d91dcc576427c4cb15
Kaan Batı <kaanbatii55@gmail.com>
2024-04-25 16:51:54 +0300

    Proje dosyası ekle.

---
+++ WebApplication2/Models/City.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.Models
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
